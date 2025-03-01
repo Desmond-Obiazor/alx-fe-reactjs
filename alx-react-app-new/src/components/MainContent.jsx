c714b9a69d63f12d777d0eb99beaa92421ab595c
Desmond-Obiazor <Stanleydesmond091@gmail.com>
2025-03-01 19:00:51 +0100

    added inline styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+        <main style = {{ backgroundColor: 'purple' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+export default MainContent;
