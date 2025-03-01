490a413f41f3a84e421902606d6c2ce0c30398e4
Desmond-Obiazor <Stanleydesmond091@gmail.com>
2025-03-01 19:42:34 +0100

    added count function

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <MainContent />
       <h1>Welcome to My React App</h1>
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
     </>
   );
